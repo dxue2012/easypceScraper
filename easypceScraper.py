2f407e8d13913827000c0b6776f4bae6e7da0959
Dyland Xue <dylandxue2012@gmail.com>
2015-03-27 00:18:43 -0400

    printing output to files

--- easypceScraper.py
+++ easypceScraper.py
@@ -10,44 +10,51 @@ from bs4 import BeautifulSoup
 import html2text
 import getpass
 
-username = raw_input("netid: ")
-password = getpass.getpass()
+def login():
+    # these fields are NOT encrypted. Be careful with them
+    username = raw_input("netid: ")
+    password = getpass.getpass()
 
-# Browser
-br = mechanize.Browser()
+    # Browser
+    br = mechanize.Browser()
 
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
 
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-br.addheaders = [('User-agent', 'Chrome')]
+    br.addheaders = [('User-agent', 'Chrome')]
 
-# The site we will navigate into, handling it's session
-br.open('http://easypce.com/')
+    # The site we will navigate into, handling it's session
+    br.open('http://easypce.com/')
 
-# View available forms
-for f in br.forms():
-    print f
+    # View available forms
+    for f in br.forms():
+        print f
 
-# Select the second (index one) form (the first form is a search query box)
-br.select_form(nr=0)
+    # Select the second (index one) form (the first form is a search query box)
+    br.select_form(nr=0)
 
-# User credentials
-br.form['username'] = username
-br.form['password'] = password
+    # User credentials
+    br.form['username'] = username
+    br.form['password'] = password
 
-# Login
-br.submit()
+    # Login
+    br.submit()
+    return br
 
-easypce = br.open('http://easypce.com/').read()
-course_510 = br.open('http://easypce.com/courses/COS510').read()
-print(easypce)
-print(course_510)
+def main():
+    br = login()
+    course_number = 'COS510'
+    course_510 = br.open('http://easypce.com/courses/' + course_number).read()
+    with open ('output/' + course_number, 'a') as f:
+        f.write(course_510)
+
+main()
