037f3ed906d1c0919ff09c5ab098bb930ccc5777
Dyland Xue <dylandxue2012@gmail.com>
2015-03-26 23:53:55 -0400

    finished log in function

--- easypceScraper.py
+++ easypceScraper.py
@@ -6,10 +6,17 @@
 ##################################### Method 1
 import mechanize
 import cookielib
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import html2text
+import getpass
 
+username = raw_input("netid: ")
+password = getpass.getpass()
+
+# Browser
 br = mechanize.Browser()
+
+# Cookie Jar
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 
@@ -21,22 +28,26 @@ br.set_handle_referer(True)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-
 br.addheaders = [('User-agent', 'Chrome')]
 
 # The site we will navigate into, handling it's session
-br.open('https://github.com/login')
+br.open('http://easypce.com/')
 
+# View available forms
 for f in br.forms():
     print f
 
+# Select the second (index one) form (the first form is a search query box)
 br.select_form(nr=0)
 
 # User credentials
-br.form['login'] = 'myusername'
-br.form['password'] = 'mypwd'
+br.form['username'] = username
+br.form['password'] = password
 
 # Login
 br.submit()
 
-br.open('github.com/settings/emails').read()
+easypce = br.open('http://easypce.com/').read()
+course_510 = br.open('http://easypce.com/courses/COS510').read()
+print(easypce)
+print(course_510)
